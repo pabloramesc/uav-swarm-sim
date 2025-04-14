532e9ab7556052438727f01d75163429d67e0a92
Pablo Ramirez <pabloramesc98@gmail.com>
2025-04-14 18:01:59 +0200

    Add avoid regions and obstacles tests for Circular, Rectangular, and Polygonal shapes

--- simulator/environment/avoid_regions/__init__.py
+++ simulator/environment/avoid_regions/__init__.py
@@ -5,7 +5,7 @@ This software is released under the MIT License.
 https://opensource.org/licenses/MIT
 """
 
-from .base import Region
+from .base import CircularRegion, PolygonalRegion, RectangularRegion, Region
 from .boundaries import (
     Boundary,
     CircularBoundary,
